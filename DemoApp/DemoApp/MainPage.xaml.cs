11fb549f7a77930e53e6c8b182ef2c3750ee64e2
Eric Zucho <ericzucho@gmail.com>
2017-10-06 15:45:54 -0500

    first commit

---
+++ DemoApp/DemoApp/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using DemoApp.WebService;
+using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DemoApp
+{
+	public partial class MainPage : ContentPage, System.ComponentModel.INotifyPropertyChanged
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            InitializeFormElements();
+        }
+
+        private void InitializeFormElements()
+        {
+            Button button = (Button)downloadButton;
+            Label label = (Label)resultLabel;
+
+            label.Text = "Please wait";
+
+            button.Clicked += async delegate
+            {
+                WebRequestService webRequestService = new WebRequestService();
+                try
+                {
+                    JsonValue jsonResponse = await webRequestService.Get("http://jsonplaceholder.typicode.com/users");
+
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        label.Text = jsonResponse.ToString();
+                    });
+
+                }
+                catch (Exception e)
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        label.Text = e.Message;
+                    });
+                }
+            };
+        }
+    }
+}
